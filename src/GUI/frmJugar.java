23edbfe58c8c7a05601e0e318e4a23fb02c02147
anacastillo <anacastillo@192.168.1.65>
2020-10-05 19:43:03 -0700

    Img lanzar cañas y re-diseño de opciones de partida.

--- src/GUI/frmJugar.java
+++ src/GUI/frmJugar.java
@@ -16,79 +16,57 @@ public class frmJugar extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanelFondo = new javax.swing.JPanel();
         lbImagenPatolli = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        btnCrearPartida = new javax.swing.JButton();
         btnUnirsePartida = new javax.swing.JButton();
+        btnCrearPartida = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Patolli");
-
-        jPanelFondo.setBackground(new java.awt.Color(255, 255, 255));
+        setBackground(new java.awt.Color(233, 216, 190));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         lbImagenPatolli.setFont(new java.awt.Font("Oriya MN", 0, 60)); // NOI18N
         lbImagenPatolli.setForeground(new java.awt.Color(3, 74, 166));
-        lbImagenPatolli.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Patolli.png"))); // NOI18N
-
-        jLabel3.setFont(new java.awt.Font("Oriya MN", 0, 60)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(3, 74, 166));
-        jLabel3.setText("Patolli Game");
-
-        btnCrearPartida.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
-        btnCrearPartida.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Crear.png"))); // NOI18N
-        btnCrearPartida.setText("Crear Partida");
-        btnCrearPartida.setIconTextGap(10);
+        lbImagenPatolli.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/patolli_florentino.jpg"))); // NOI18N
+        getContentPane().add(lbImagenPatolli, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 120, -1, 280));
 
         btnUnirsePartida.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
         btnUnirsePartida.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Unirse.png"))); // NOI18N
         btnUnirsePartida.setText("Unirse Partida");
         btnUnirsePartida.setIconTextGap(10);
+        getContentPane().add(btnUnirsePartida, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 240, 200, 54));
 
-        javax.swing.GroupLayout jPanelFondoLayout = new javax.swing.GroupLayout(jPanelFondo);
-        jPanelFondo.setLayout(jPanelFondoLayout);
-        jPanelFondoLayout.setHorizontalGroup(
-            jPanelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelFondoLayout.createSequentialGroup()
-                .addGap(136, 136, 136)
-                .addComponent(jLabel3)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanelFondoLayout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addGroup(jPanelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnUnirsePartida, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnCrearPartida, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 94, Short.MAX_VALUE)
-                .addComponent(lbImagenPatolli)
-                .addGap(24, 24, 24))
+        btnCrearPartida.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        btnCrearPartida.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Crear.png"))); // NOI18N
+        btnCrearPartida.setText("Crear Partida");
+        btnCrearPartida.setIconTextGap(10);
+        getContentPane().add(btnCrearPartida, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, 203, 53));
+
+        jLabel3.setFont(new java.awt.Font("Oriya MN", 0, 48)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Opciones de partida");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 20, -1, -1));
+
+        jLabel1.setBackground(new java.awt.Color(233, 216, 190));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 640, 420));
+
+        jPanel1.setBackground(new java.awt.Color(233, 216, 190));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 660, Short.MAX_VALUE)
         );
-        jPanelFondoLayout.setVerticalGroup(
-            jPanelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelFondoLayout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(jLabel3)
-                .addGroup(jPanelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanelFondoLayout.createSequentialGroup()
-                        .addGap(70, 70, 70)
-                        .addComponent(btnCrearPartida, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnUnirsePartida, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanelFondoLayout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(lbImagenPatolli, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(34, Short.MAX_VALUE))
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 420, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelFondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelFondo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 660, 420));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -96,8 +74,9 @@ public class frmJugar extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCrearPartida;
     private javax.swing.JButton btnUnirsePartida;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanelFondo;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lbImagenPatolli;
     // End of variables declaration//GEN-END:variables
 }
