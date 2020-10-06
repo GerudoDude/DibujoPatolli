a551d1d49de70b30b689c99148f024783dd0c7e5
cr-ls <71845505+cr-ls@users.noreply.github.com>
2020-10-05 17:59:09 -0700

    Se modifico el nombre del frmPrincipal a frmJugar para evitar confusiones en un futuro, tambien se creo una nueva clase llamada run, cuya funcion es ejecutar dicho frame.

---
+++ src/GUI/run.java
@@ -0,0 +1,14 @@
+package GUI;
+
+/**
+ *
+ * @author Equipo 5
+ */
+public class run {
+    /* Metodo que corre el frame Jugar */
+    public static void main(String[] args) {
+        frmJugar jugar = new frmJugar();
+        
+        jugar.setVisible(true);
+    }  
+}
