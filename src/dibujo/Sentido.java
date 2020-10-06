f742e68a1ab5dc144647a854f281629aefead992
cr-ls <71845505+cr-ls@users.noreply.github.com>
2020-10-05 17:06:34 -0700

    Se documento la clase sentido.java

--- src/dibujo/Sentido.java
+++ src/dibujo/Sentido.java
@@ -5,7 +5,8 @@ package dibujo;
  * @author Equipo 5
  */
 
-/* Clase que  */
+/* Clase que ayuda a crear la casilla del triangulo, dando el sentido que 
+   tendra el triangulo segun su ubicacion en el tablero */
 public enum Sentido {
     ARRIBA,
     ABAJO,
